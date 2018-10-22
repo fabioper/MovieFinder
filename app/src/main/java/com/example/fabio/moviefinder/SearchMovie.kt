0a3932eb03f10f954e4340e777db28d3e78dae13
Fábio Pereira <fabiopereira.fps@gmail.com>
2018-10-22 00:19:32 -0300

    Increase bottom padding of recyclerview

---
+++ app/src/main/java/com/example/fabio/moviefinder/SearchMovie.kt
@@ -0,0 +1,12 @@
+package com.example.fabio.moviefinder
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchMovie : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_movie)
+    }
+}
