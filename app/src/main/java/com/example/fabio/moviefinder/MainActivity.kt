49be19eab652962674a49b44cf3363ffe816cc43
Fábio Pereira <fabiopereira.fps@gmail.com>
2018-10-22 01:11:24 -0300

    Comment MainActivity actions

--- app/src/main/java/com/example/fabio/moviefinder/MainActivity.kt
+++ app/src/main/java/com/example/fabio/moviefinder/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.fabio.moviefinder
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.GridLayoutManager
@@ -10,11 +11,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        // Get TMDBService object instance
         val moviesService = MovieFinderService.moviesService
+
+        // Get upcoming movies from TMDBService
         val movies = moviesService.getUpcomingMovies(0).results
 
+        // Set up RecyclerView
+        // Define the RecyclerView layout manager as a GridLayout with a span of 2
         recyclerView.layoutManager = GridLayoutManager(this, 2)
+
+        // Assign the RecyclerView adapter
         recyclerView.adapter = UpcomingMoviesAdapter(this, movies)
+
+        // Set up FAB click listener to move to Search Movie activity
+        floatingActionButton.setOnClickListener {
+            val intent = Intent(this, SearchMovie::class.java)
+            startActivity(intent)
+        }
     }
 }
